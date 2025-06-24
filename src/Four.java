8148b0d3f4821373c0cd65d3286bee8f8c26a44d
USER <rhs9827@naver.com>
2025-06-24 16:10:54 +0900

    first commit

---
+++ src/Four.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Four {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int answer;
+        int number;
+        int count = 0;
+
+        answer = (int)(Math.random() * 100 + 1);
+
+
+            System.out.printf("정답을 맞춰주세요.");
+            number= scanner.nextInt();
+
+        for(;;) {
+            if(number < answer) {
+                System.out.println("\n" + number + "는 정답보다 작습니다.");
+                count++;
+            } else if (number > answer) {
+                System.out.println("\n" + number + "는 정답보다 큽니다.");
+                count++;
+            } else {
+                System.out.println("\n" + "정답입니다.");
+                count++;
+                break;
+            }
+            System.out.printf("다시 맞춰주세요.");
+            number= scanner.nextInt();
+        }
+        System.out.println("정답 시도 횟수는 " + count + "번 입니다.");
+    }
+}
