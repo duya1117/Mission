c2bbcb0a5febb334d7b8178113936e90d5bbbfe1
USER <rhs9827@naver.com>
2025-06-24 18:07:53 +0900

    commit

--- src/Seven.java
+++ src/Seven.java
@@ -1,4 +1,37 @@
+import java.util.Scanner;
+
 public class Seven {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        System.out.println("1~7의 숫자를 입력해주세요");
+        number = scanner.nextInt();
+
+        switch (number){
+            case 1:
+                System.out.println("월요일");
+                break;
+            case 2:
+                System.out.println("화요일");
+                break;
+            case 3:
+                System.out.println("수요일");
+                break;
+            case 4:
+                System.out.println("목요일");
+                break;
+            case 5:
+                System.out.println("금요일");
+                break;
+            case 6:
+                System.out.println("토요일");
+                break;
+            case 7:
+                System.out.println("일요일");
+                break;
+            default:
+                System.out.println("잘못 입력하셨습니다.");
+        }
     }
 }
